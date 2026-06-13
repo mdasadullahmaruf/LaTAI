14624b19d558e63618b56a84794fc9265de1b50d
mdasadullahmaruf <mdasadullahmaruf1@gmail.com>
2026-06-13 23:37:22 +0400

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application") version "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
+}
+
+android {
+    namespace = "com.majesty.lat"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.majesty.lat"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
